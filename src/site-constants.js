130b37829e56729ba4779ddda59ef0c9b2b8bd94
vicanso <vicansocanbico@gmail.com>
2018-02-10 09:54:41 +0800

    update docs

--- src/site-constants.js
+++ src/site-constants.js
@@ -7,8 +7,8 @@
 
 // NOTE: We can't just use `location.toString()` because when we are rendering
 // the SSR part in node.js we won't have a proper location.
-const urlRoot = 'https://reactjs.org';
-const version = '0.1.0';
+const urlRoot = 'http://pike.aslant.site';
+const version = '0.1.1';
 const babelURL = '//unpkg.com/babel-standalone@6.26.0/babel.min.js';
 
 export {urlRoot, version, babelURL};
